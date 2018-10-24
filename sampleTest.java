da4ce3fceda62315a94e4292f77339c6843187d8
Adrian Rivera-Zayas <adrianriveraza@gmail.com>
2018-10-24 12:45:06 -0500

    first commit

---
+++ sampleTest.java
@@ -0,0 +1,74 @@
+public class sampleTest {
+
+    public static void main(String[] args) {
+
+        int a;
+        long b;
+        double c;
+        float d;
+
+        a = a + 1;
+        a = a * 1;
+        a = a - 1;
+        a = a / 1;
+        a = a % 1;
+
+        b = b + 1;
+        b = b * 1;
+        b = b - 1;
+        b = b / 1;
+        b = b % 1;
+
+        c = c + 1;
+        c = c * 1;
+        c = c - 1;
+        c = c / 1;
+        c = c % 1;
+
+        d = d + 1;
+        d = d * 1;
+        d = d - 1;
+        d = d / 1;
+        d = d % 1;
+        if (a == 1) {
+            a++;
+        }
+        if (a != 1) {
+            a++;
+        }
+        if (a > 1) {
+            a++;
+        }
+        if (a < 1) {
+            a++;
+        }
+        if (a >= 1) {
+            a++;
+        }
+        if (a <= 1) {
+            a++;
+        }
+        
+        if (a == 0) {
+            a++;
+        }
+        if (a != 0) {
+            a++;
+        }
+        if (a > 0) {
+            a++;
+        }
+        if (a < 0) {
+            a++;
+        }
+        if (a >= 0) {
+            a++;
+        }
+        if (a <= 0) {
+            a++;
+        }
+
+        System.out.println("Hello, World");
+    }
+
+}
